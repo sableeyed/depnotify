7cfd987781d43e17da89e87f58c90f0dabe493ff
mlizbeth <1906931+mlizbeth@users.noreply.github.com>
2019-03-25 17:00:21 -0500

    Complete Workflow
    Ready for real machines, VM tests passed successfully.

--- depNotify.sh
+++ depNotify.sh
@@ -1,4 +1,4 @@
-### This is not production ready yet ###
+# 3/25/19 Initial Release v1
 
 #!/bin/bash
 setupDone="/Library/Application\ Support/Jamf/setupDone"
@@ -11,21 +11,96 @@ configList="/Users/$CURRENTUSER/Library/Preferences/menu.nomad.DEPNotify.plist"
 BANNER_IMG="/var/tmp/banner.png"
 REGISTRATION_DONE="/var/tmp/com.depnotify.registration.done"
 
+BUILDING_ARRAY=(
+	"Chapman Center"
+    "Coates University Center"
+    "Center for Science & Innovation"
+    "Dicke Smith Building"
+    "Elizabeth Huth Coates Library"
+    "Halsell Administrative Studies"
+    "Holt Conference Center"
+    "King's Court"
+    "Laurie Auditorium"
+    "Marrs McLean Hall"
+    "Margarite B. Parker Chapel"
+    "Northrup Hall"
+    "Richardson Communication Center"
+    "Ruth Taylor Theatre"
+    "Storch Memorial Building"
+    "William Bell Center"
+	)
+
+DEPARTMENT_ARRAY=(
+	"Academic Affairs"
+    "Academic Support"
+    "Admissions"
+    "Alumni Relations & Development"
+    "Art & Art History"
+    "Athletics"
+    "Biology"
+    "Business Office"
+    "Chemistry"
+    "Classical Studies"
+    "Collaborative for Learning & Teaching"
+    "Communication"
+    "Computer Science"
+    "Counseling Services"
+    "Dean of Students"
+    "Economics"
+    "Education"
+    "Endowments"
+    "Engineering"
+    "English"
+    "Environmental Health & Safety"
+    "Facilities Services"
+    "Finance & Administration"
+    "Geosciences"
+    "Health Care Administration"
+    "Health Services"
+    "History"
+    "Human Communication & Theatre"
+    "Human Resources"
+    "Information Technology Services"
+    "Library"
+    "Mathematics"
+    "Modern Languages & Literatures"
+    "Music"
+    "Philosophy"
+    "Physics & Astronomy"
+    "Political Science"
+    "President's Office"
+    "Psychology"
+    "Purchasing"
+    "Registrar"
+    "Religion"
+    "Residential Life"
+    "Risk Management & Insurance"
+    "School of Business"
+    "Sociology & Anthropology"
+    "Strategic Communications & Marketing"
+    "Student Financial Services"
+    "Tiger Card Office"
+    "Trinitonian"
+    "Trinity University Press"
+    "TUPD"
+    "Other"
+	)
+
 POLICY_ARRAY=(
     #"Binding to Active Directory,ADBIND"
-  	#"Installing Google Chrome,CHROME"
-  	#"Installing Mozilla Firefox,FIREFOX"
-  	#"Installing VLC Media Player,VLC"
-  	#"Installing Java Runtime Environment,JRE"
-  	#"Installing Adobe Flash Player,FLASH"
-    #"Installing Microsoft Office 2019,O2019"
-    #"Installing BitDefender,BDFS"
-    #"Enabling Remote Management,SCRIPTS"
-    #"Installing Updates,UPDATES"
-    #"Installing KACE Agent,KACE"
+  	"Installing Google Chrome,CHROME"
+  	"Installing Mozilla Firefox,FIREFOX"
+  	"Installing VLC Media Player,VLC"
+  	"Installing Java Runtime Environment,JRE"
+  	"Installing Adobe Flash Player,FLASH"
+    "Installing Microsoft Office 2019,O2019"
+    "Installing BitDefender,BDFS"
+    "Installing KACE Agent,KACE"
+    "Enabling Remote Management,SCRIPTS"
+    "Installing OS Updates,UPDATES"
 )
 
-#if [ -f "${setupDone}" ]; then exit 0; fi
+if [ -f "${setupDone}" ]; then exit 0; fi
 
 if pgrep -x "Finder" \
 	&& pgrep -x "Dock" \
@@ -35,10 +110,7 @@ if pgrep -x "Finder" \
 		/usr/bin/caffeinate -d -i -m -u -s &
 		caffeinatepid=$!
 
-		#killall Installer
-		echo "Hello"
-
-		sleep 5
+		killall Installer
 
 		# Register input plist 
 		sudo -u "$CURRENTUSER" defaults write "$configList" pathToPlistFile "$inputList"
@@ -68,31 +140,90 @@ if pgrep -x "Finder" \
 		sudo -u "$CURRENTUSER" defaults write "$configList" textField2IsOptional -bool true
 
 		sudo -u "$CURRENTUSER" defaults write "$configList" popupButton1Label "Building"
-		sudo -u "$CURRENTUSER" defaults write "$configList" popupButton1Content -array "ayy" "lmao"
+		for BUILDING_ARRAY in "${BUILDING_ARRAY[@]}"; do
+			sudo -u "$CURRENTUSER" defaults write "$configList" popupButton1Content -array-add "$BUILDING_ARRAY"
+		done
 
 		sudo -u "$CURRENTUSER" defaults write "$configList" popupButton2Label "Department"
-		sudo -u "$CURRENTUSER" defaults write "$configList" popupButton2Content -array "420" "xd"
+		for DEPARTMENT_ARRAY in "${DEPARTMENT_ARRAY[@]}"; do
+			sudo -u "$CURRENTUSER" defaults write "$configList" popupButton2Content -array-add "$DEPARTMENT_ARRAY"
+		done
 
 
 		# Open DepNotify
 	    sudo -u "$CURRENTUSER" /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify &
-
+	    
 	    while [ ! -f "$REGISTRATION_DONE" ]; do
-	    	echo "$(date "+%a %h %d %H:%M:%S"): Waiting on conpletion of registration" >> $dLOG
-	    	sleep 1
+	    	echo "$(date "+%a %h %d %H:%M:%S"): Waiting on completion of registration" >> $dLOG
+	    	sleep 2
 	    done
 
-	    #Read and Set Computer Name/Asset Tag/Building/Department
+	    #Computer Name Logic
+	    REG_FIELD_1_VALUE=$(defaults read "$inputList" "Device Name") #This field is mandatory
+	    if [ ! "$REG_FIELD_1_VALUE" = "" ]; then
+	    	echo "Status: Setting computer name to $REG_FIELD_1_VALUE" >> $dLOG
+	    	scutil --set HostName "$REG_FIELD_1_VALUE"
+	    	scutil --set LocalHostName "$REG_FIELD_1_VALUE"
+	    	scutil --set ComputerName "$REG_FIELD_1_VALUE"
+	    	"$JAMF_BINARY" setComputerName -name "$REG_FIELD_1_VALUE"
+	    else
+	    	echo "Status: Something went wrong because DEVICE_NAME can't be empty." >> $dLOG
+	    	exit 1
+	    fi
 
-	    #Update the statuses appropriately
+	    # Asset Tag Logic
+	    REG_FIELD_2_VALUE=$(defaults read "$inputList" "Asset Tag")
+	    REG_FIELD_2_OPTIONAL=$(defaults read "$configList" "textField2IsOptional")
+	    if [ "$REG_FIELD_2_OPTIONAL" = 1 ] && [ "$REG_FIELD_2_VALUE" = "" ]; then
+	    	echo "Status: Asset tag was left empty. Skipping..." >> $dLOG
+	    	sleep 2
+	    else #set the asset tag
+	    	echo "Status: Setting asset tag to $REG_FIELD_2_VALUE." >> $dLOG
+	    	"$JAMF_BINARY" recon -assetTag "$REG_FIELD_2_VALUE"
+	    fi
 
+	    #Device Building Logic
+	    REG_FIELD_3_VALUE=$(defaults read "$inputList" "Building")
+	    if [ ! "$REG_FIELD_3_VALUE" = "" ]; then
+	    	echo "Status: Setting building to $REG_FIELD_3_VALUE" >> $dLOG
+	    	"$JAMF_BINARY" recon -building "$REG_FIELD_3_VALUE"
+	    else
+	    	echo "Something went wrong: Building Logic." >> $dLOG
+	    	exit 1
+	    fi
+
+
+	    #Device Department Logic
+	    REG_FIELD_4_VALUE=$(defaults read "$inputList" "Department")
+	    if [ ! "$REG_FIELD_4_VALUE" = "" ]; then
+	    	echo "Status: Setting department to $REG_FIELD_4_VALUE" >> $dLOG
+	    	"$JAMF_BINARY" recon -department "$REG_FIELD_4_VALUE"
+	    else
+	    	echo "Something went wrong: Department Logic" >> $dLOG
+	    	exit 1
+	    fi
+
+
+	    #Begin device imaging
+	    for POLICY in "${POLICY_ARRAY[@]}"; do
+  			echo "Status: $(echo "$POLICY" | cut -d ',' -f1)" >> "$dLOG"
+     		"$JAMF_BINARY" policy -event "$(echo "$POLICY" | cut -d ',' -f2)"
+		done
+
+		touch $setupDone
+		touch /var/db/receipts/edu.trinity.imaging.bom
+		echo "Status: Updating device inventory" >> $dLOG
+		$JAMF_BINARY recon
+		echo "Status: Cleaning up files and restarting the system" >> $dLOG
+		sleep 2
+		kill $caffeinatepid
+		rm -fr /Library/LaunchDaemons/edu.trinity.launch.plist
+		rm -fr $inputList
+		rm -fr $configList
+		rm -fr /var/tmp/banner.png
+		echo "Command: RestartNow:" >> $dLOG
+
+		rm -fr /Applications/Utilities/DEPNotify.app
+		rm -- "$0"
 fi
-
-
-# Loop to run policies
-# for POLICY in "${POLICY_ARRAY[@]}"; do
-#   echo "Status: $(echo "$POLICY" | cut -d ',' -f1)" >> "$dLOG"
-#     "$JAMF_BINARY" policy -event "$(echo "$POLICY" | cut -d ',' -f2)"
-# done
-
-
+exit 0
--- postinstall.sh
+++ postinstall.sh
@@ -2,4 +2,4 @@
 chmod a+x /var/tmp/depNotify.sh
 chmod 644 /Library/LaunchDaemons/edu.trinity.launch.plist
 chown root:wheel /Library/LaunchDaemons/edu.trinity.launch.plist
-launchctl load /Library/LaunchDaemons/edu.trinity.launch.plist
+launchctl bootstrap system /Library/LaunchDaemons/edu.trinity.launch.plist
