2e8fb98ce6efb57f9e8f44bb53f57b657f1014cf
mlizbeth <1906931+mlizbeth@users.noreply.github.com>
2019-07-26 09:59:21 -0500

    extension attribute to populate "Installation Date" field
    Uses the day that DEPNotify ran as the "Installation" or "Deployment" date of the machine. Logic to not run if the value exists.
    Need to account for older files/paths.
    Need to account for lab machines.

---
+++ installDate_EA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#@Author mlizbeth 
+#@Date 7-26-19
+#@Version 1.0
+#FACULTY/STAFF ONLY
+#TODO change lab imaging to have the same identifier
+apiUser="$4"
+apiPass="$5"
+jssURL=$(/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url | /usr/bin/sed s'/.$//')
+udid=$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/UUID/ { print $3; }')
+install_date=$(/usr/bin/curl -X GET --user "$apiUser:$apiPass" "$jssURL/JSSResource/computers/udid/$udid" | /usr/bin/xmllint --xpath "//name[text()='Installation Date']/following-sibling::value/text()" -)
+if [ ! "$install_date" ]; then #find install date from file
+	if [ -f /var/db/receipts/edu.trinity.enrollment.plist ]; then
+		date=$(/usr/bin/defaults read /var/db/receipts/edu.trinity.enrollment.plist InstallDate | awk '{print $1"\ "$2}')
+		/bin/echo "<result>$date</result>"
+	fi
+fi 
+# if already populated, do nothing
