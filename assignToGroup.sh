7277362bbda54f3cadc5745dc19644796894dd20
Madeline <1906931+mlizbeth@users.noreply.github.com>
2019-08-12 14:39:49 -0500

    Add a computer to a static group

---
+++ assignToGroup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apiUser="user"
+apiPass="pass"
+jssURL=$(/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url | /usr/bin/sed s'/.$//')
+groupID="##"
+apiURL="JSSResource/computergroups/id/${groupID}"
+computerName=$(hostname)
+xmlHeader="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
+apiData="<computer_group><id>${groupID}</id><name>Group name</name><computer_additions><computer><name>$computerName</name></computer></computer_additions></computer_group>"
+
+curl -sSkiu ${apiUser}:${apiPass} "${jssURL}/${apiURL}" \
+    -H "Content-Type: text/xml" \
+    -d "${xmlHeader}${apiData}" \
+    -X PUT  > /dev/null
